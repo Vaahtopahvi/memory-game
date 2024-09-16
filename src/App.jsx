cf212fce3ae94ec3ab8ac73550d2bca0ac5bf938
vaahtopahvi <kermakahvi@hotmail.com>
2024-09-16 21:31:48 +0300

    add a loading state and clean some code

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,14 @@ function App() {
   const [score, setScore] = useState(0);
   const [bestScore, setBestScore] = useState(0);
   const [help, setHelp] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   const capitalizeFirstLetter = (string) => {
     return string.charAt(0).toUpperCase() + string.slice(1);
   };
 
   const fetchPokemon = async () => {
+    setLoading(true);
     const pokemonList = [];
     const favPokemons = [
       395, 552, 95, 94, 79, 464, 108, 383, 61, 260, 291, 411,
@@ -31,6 +33,7 @@ function App() {
     }
 
     setPokemon(pokemonList);
+    setLoading(false);
   };
 
   //execute fetchPokemon when the component mounts
@@ -71,8 +74,7 @@ function App() {
   return (
     <>
       <div className="min-h-screen flex flex-col items-center justify-center gap-3 sm:gap-5 bg-gradient-to-r from-primary to-secondary p-4">
-        
-        <div className="flex flex-col items-center justify-center text-center backdrop-blur-xl bg-black/70 p-3 sm:p-5 rounded-xl">
+        <div className="flex flex-col items-center justify-center text-center bg-black/70 p-3 sm:p-5 rounded-xl">
           <h1 className="font-mono font-bold text-3xl text-white sm:text-4xl md:text-5xl mb-2 sm:mb-3 p-2 sm:p-5">
             Pokémon Memory Game
           </h1>
@@ -81,44 +83,60 @@ function App() {
             <p>Best Score: {bestScore}</p>
           </div>
         </div>
-        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-2 sm:gap-4 w-full max-w-3xl">
-          {
-            //map through each pokemon in an array and render them
-            pokemon.map((poke) => {
-              return (
-                //render each pokemon as a card
-                <div
-                  className="bg-white rounded-md shadow-md p-2 sm:p-4 cursor-pointer transform hover:scale-105 transition-transform"
-                  key={poke.id}
-                  onClick={() => handleClick(poke.id)}
-                >
-                  {/* give each pokemon image & name */}
-                  <img
-                    src={poke.img}
-                    alt=""
-                    className="w-16 h-16 sm:w-24 sm:h-24 md:w-32 md:h-32 mx-auto"
-                  />
-                  <p className="text-center mt-1 sm:mt-2 text-sm sm:text-base">
-                    {poke.name}
-                  </p>
-                </div>
-              );
-            })
-          }
+        <div
+          className={`w-full max-w-3xl ${
+            loading
+              ? "flex items-center justify-center h-64"
+              : "grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-2 sm:gap-4"
+          }`}
+        >
+          {loading ? (
+            <p className="text-black text-lg sm:text-xl text-center">
+              Loading...
+            </p>
+          ) : (
+            <>
+              {/* //map through each pokemon in an array and render them */}
+              {pokemon.map((poke) => {
+                return (
+                  //render each pokemon as a card
+                  <div
+                    className="bg-white rounded-md shadow-md p-2 sm:p-4 cursor-pointer transform hover:scale-105 transition-transform"
+                    key={poke.id}
+                    onClick={() => handleClick(poke.id)}
+                  >
+                    {/* give each pokemon image & name */}
+                    <img
+                      src={poke.img}
+                      alt=""
+                      className="w-16 h-16 sm:w-24 sm:h-24 md:w-32 md:h-32 mx-auto"
+                    />
+                    <p className="text-center mt-1 sm:mt-2 text-sm sm:text-base">
+                      {poke.name}
+                    </p>
+                  </div>
+                );
+              })}
+            </>
+          )}
         </div>
-        
-          <div className="fixed right-5 bottom-5">
+
+        <div className="fixed right-5 bottom-5">
           <img
             src={helpIcon}
             alt="help"
             className="h-10 w-10 cursor-pointer"
-            onClick={toggleInfo}/>
-            {help && (
-          <div className="absolute right-12 bottom-12 min-w-56 bg-white p-4 rounded shadow-lg">
-            <p className="text-black">This is a Pokémon memory game. Click on one of my favorite Pokémons to earn points, but don&apos;t click on the same Pokémon twice!</p>
-          </div>
-        )}
-          
+            onClick={toggleInfo}
+          />
+          {help && (
+            <div className="absolute right-12 bottom-12 min-w-56 bg-slate-100 p-4 rounded shadow-lg">
+              <p className="text-black">
+                This is a Pokémon memory game. Click on one of my favorite
+                Pokémons to earn points, but don&apos;t click on the same
+                Pokémon twice!
+              </p>
+            </div>
+          )}
         </div>
       </div>
     </>
